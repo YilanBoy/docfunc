43712349f5d7bf39580569f66f92a9252f511e08
Allen <aq98723il6732@gmail.com>
2020-10-01 13:13:34 +0800

    聲明 tagJsonToArray function 返回值的類型為 array

--- app/Services/DataTransService.php
+++ app/Services/DataTransService.php
@@ -5,7 +5,8 @@ namespace App\Services;
 class DataTransService
 {
     // 將 tag 的 JSON 資料轉成 array，這裡的問號，代表允許參數為 null
-    public function tagJsonToArray(?string $tagJson)
+    // 後面的 : array 為聲明這個 function 返回的值為 array 類型
+    public function tagJsonToArray(?string $tagJson): array
     {
         $tagArray = [];
 

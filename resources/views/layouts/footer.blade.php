1e959e852713f0d9575743683e5a4451fd7e6011
Allen <aq98723il6732@gmail.com>
2020-11-08 21:17:05 +0800

    調整 footer 的標籤與連結

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,18 +1,26 @@
 {{-- footer --}}
 <footer class="footer">
-    <div class="container mt-3">
-        <p class="float-left text-white">
-            Logo made by
-            <a href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank" rel="nofollow noopener noreferrer">Freepik</a>
-            from
-            <a href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="nofollow noopener noreferrer">www.flaticon.com</a>
-        </p>
+    <div class="container py-2">
+        <div class="float-left text-white">
+            <span>
+                <a class="text-white" href="https://github.com/YilanBoy/personal-blog" target="_blank" rel="nofollow noopener noreferrer">網站壽司扣（GitHub）</a>
+            </span>
+        </div>
 
-        <p class="float-right text-white">
-            特別感謝
-            <a href="https://learnku.com/laravel" target="_blank" rel="nofollow noopener noreferrer">Laravel China</a>
-            與
-            <a href="https://www.facebook.com/groups/laravel.tw/" target="_blank" rel="nofollow noopener noreferrer">Laravel 台灣</a>
-        </p>
+        <div class="float-right text-white">
+            <span>
+                特別感謝
+                <a class="text-white" href="https://learnku.com/laravel" target="_blank" rel="nofollow noopener noreferrer">Laravel China</a>
+                與
+                <a class="text-white" href="https://www.facebook.com/groups/laravel.tw/" target="_blank" rel="nofollow noopener noreferrer">Laravel 台灣</a>
+            </span>
+            <br>
+            <span>
+                Logo made by
+                <a class="text-white" href="https://www.flaticon.com/authors/freepik" title="Freepik" target="_blank" rel="nofollow noopener noreferrer">Freepik</a>
+                from
+                <a class="text-white" href="https://www.flaticon.com/" title="Flaticon" target="_blank" rel="nofollow noopener noreferrer">www.flaticon.com</a>
+            </span>
+        </div>
     </div>
 </footer>

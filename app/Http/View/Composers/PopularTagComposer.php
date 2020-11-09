b0dfaf8afe51c4198cdc8f8e8f926a594a32bb91
Allen <aq98723il6732@gmail.com>
2020-11-09 21:22:34 +0800

    調整程式碼縮排

--- app/Http/View/Composers/PopularTagComposer.php
+++ app/Http/View/Composers/PopularTagComposer.php
@@ -24,7 +24,11 @@ class PopularTagComposer
         $popularTags = Cache::remember('popularTags', 86400, function () {
 
             // 取出標籤使用次數前 20 名
-            $tagCount = $this->tag->withCount('posts')->having('posts_count', '>', 0)->orderByDesc('posts_count')->limit(20)->get();
+            $tagCount = $this->tag->withCount('posts')
+                ->having('posts_count', '>', 0)
+                ->orderByDesc('posts_count')
+                ->limit(20)
+                ->get();
 
             return $tagCount;
         });

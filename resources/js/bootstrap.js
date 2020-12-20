611f5f4766a02714d462b2b7505888365b9aae92
Allen <aq98723il6732@gmail.com>
2020-12-20 11:30:18 +0800

    調整載入 node_module 的程式碼

--- resources/js/bootstrap.js
+++ resources/js/bootstrap.js
@@ -1,18 +1,6 @@
 window._ = require('lodash');
 
 /**
- * We'll load the Bootstrap 5 Beta which provides support
- * for JavaScript based Bootstrap features such as modals and tabs. This
- * code may be modified to fit the specific needs of your application.
- */
-
-try {
-    window.Popper = require('@popperjs/core').default;
-
-    require('bootstrap');
-} catch (e) {}
-
-/**
  * We'll load the axios HTTP library which allows us to easily issue requests
  * to our Laravel back-end. This library automatically handles sending the
  * CSRF token as a header based on the value of the "XSRF" token cookie.
@@ -38,6 +26,3 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 //     cluster: process.env.MIX_PUSHER_APP_CLUSTER,
 //     forceTLS: true
 // });
-
-// 引入 sweetalert 2
-window.swal = require('sweetalert2');

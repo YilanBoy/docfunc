4f3c535e6e5477eb3ce61e0aec9aeec72784cc6f
Allen <aq98723il6732@gmail.com>
2021-01-15 20:54:47 +0800

    調整 app.js 中 bootstrap 5 的載入方式

--- resources/js/app.js
+++ resources/js/app.js
@@ -3,7 +3,7 @@ require('./bootstrap');
 // popper
 window.popper = require('@popperjs/core');
 // bootstrap 5
-require('bootstrap');
+window.bootstrap = require('bootstrap');
 
 // sweetalert 2
 window.swal = require('sweetalert2');

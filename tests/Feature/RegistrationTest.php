fccbdf651254d290c54bda09f3538ddc2a1bc2df
Allen <aq98723il6732@gmail.com>
2021-05-01 17:22:41 +0800

    調整註冊會員的自動測試

--- tests/Feature/RegistrationTest.php
+++ tests/Feature/RegistrationTest.php
@@ -20,13 +20,13 @@ class RegistrationTest extends TestCase
     public function test_new_users_can_register()
     {
         $response = $this->post('/register', [
-            'name' => 'Test User',
+            'name' => 'Test_User',
             'email' => 'test@example.com',
             'password' => 'password',
             'password_confirmation' => 'password',
         ]);
 
         $this->assertAuthenticated();
-        $response->assertRedirect(RouteServiceProvider::HOME);
+        $response->assertRedirect('verify-email');
     }
 }

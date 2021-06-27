95af6b887519c80c7a580cf45125d17dc8c9d299
Allen <aq98723il6732@gmail.com>
2021-06-27 20:35:02 +0800

    調整文章列表的自動測試

--- tests/Feature/PostTest.php
+++ tests/Feature/PostTest.php
@@ -2,6 +2,9 @@
 
 namespace Tests\Feature;
 
+use App\Http\Livewire\Posts;
+use App\Models\Category;
+use Livewire\Livewire;
 use App\Models\User;
 use App\Models\Post;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -14,9 +17,45 @@ class PostTest extends TestCase
 
     public function test_posts_index_can_be_rendered()
     {
-        $response = $this->get('/');
+        $user = User::factory()->create();
 
-        $response->assertStatus(200);
+        $posts = Post::factory(10)->create(
+            [
+                'category_id' => rand(1, 3),
+                'user_id' => $user->id,
+            ]
+        );
+
+        $this->get(route('posts.index'))
+            ->assertStatus(200)
+            ->assertSeeLivewire('posts');
+    }
+
+    public function test_category_can_filter_posts()
+    {
+        $user = User::factory()->create();
+
+        $categoryOnePost = Post::factory()->make(
+            [
+                'title' => 'this post belongs to category one',
+                'user_id' => $user->id,
+                'category_id' => 1,
+            ]
+        );
+
+        $categoryOnePost->save();
+
+        Livewire::test(Posts::class)
+            ->set('category', Category::first())
+            ->assertViewHas('posts', function ($posts) {
+                return $posts->count() === 1;
+            });
+
+        Livewire::test(Posts::class)
+            ->set('category', Category::find(2))
+            ->assertViewHas('posts', function ($posts) {
+                return $posts->count() === 0;
+            });
     }
 
     public function test_user_can_view_a_post()

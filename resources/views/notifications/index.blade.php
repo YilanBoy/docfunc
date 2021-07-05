b71dd7946fbf9b1380ab1f540707f51f8dac4d8f
Allen <aq98723il6732@gmail.com>
2021-07-05 14:22:31 +0800

    調整通知頁面樣式

--- resources/views/notifications/index.blade.php
+++ resources/views/notifications/index.blade.php
@@ -62,7 +62,7 @@
                                     </div>
 
                                     {{-- 通知時間 --}}
-                                    <div class="mt-4 text-sm text-gray-600">
+                                    <div class="mt-4 text-sm text-gray-400">
                                         <i class="bi bi-clock-fill"></i>
                                         <span class="ml-2" title="{{ $notification->created_at }}">
                                             {{ $notification->created_at->diffForHumans() }}
@@ -89,67 +89,4 @@
             </div>
         </div>
     </div>
-    {{-- <div class="container mb-5">
-        <div class="row justify-content-md-center">
-            <div class="col-12 col-xl-8">
-
-                <div class="card shadow">
-                    <h5 class="card-header py-3"><i class="far fa-bell" aria-hidden="true"></i> 我的通知</h5>
-                    <div class="card-body">
-                        @if ($notifications->count())
-                            <ul class="list-group list-group-flush">
-                                @foreach ($notifications as $notification)
-                                    <!-- 通知列表區塊 -->
-                                    <li class="list-group-item px-0">
-                                        <div class="row p-2">
-                                            <!-- 大頭貼 -->
-                                            <div class="col-2 d-flex justify-content-center align-items-center">
-                                                <a href="{{ route('users.show', ['user' => $notification->data['user_id']]) }}">
-                                                    <img class="rounded-circle"
-                                                    alt="{{ $notification->data['user_name'] }}"
-                                                    src="{{ $notification->data['user_avatar'] }}"
-                                                    width="48px" height="48px">
-                                                </a>
-                                            </div>
-
-                                            <!-- 回覆內容 -->
-                                            <div class="col-8">
-                                                <div class="mb-1">
-                                                    <a class="text-decoration-none"
-                                                    href="{{ route('users.show', ['user' => $notification->data['user_id']]) }}">{{ $notification->data['user_name'] }}</a>
-                                                    回覆
-                                                    <a class="text-decoration-none"
-                                                    href="{{ $notification->data['post_link'] }}">{{ $notification->data['post_title'] }}</a>
-                                                </div>
-                                                <div class="card-text">
-                                                    {!! $notification->data['reply_content'] !!}
-                                                </div>
-                                            </div>
-
-                                            <!-- 回覆時間 -->
-                                            <div class="">
-                                                <span title="{{ $notification->created_at }}">
-                                                    <i class="bi bi-clock-fill"></i>
-                                                    <span class="ml-2">
-                                                        {{ $notification->created_at->diffForHumans() }}
-                                                    </span>
-                                                </span>
-                                            </div>
-                                        </div>
-                                    </li>
-                                @endforeach
-
-                                {{ $notifications->links() }}
-                            </ul>
-                        @else
-                            <div class="d-flex justify-content-center p-5">
-                                <label class="fs-3">沒有消息通知！</label>
-                            </div>
-                        @endif
-                    </div>
-                </div>
-
-            </div>
-        </div>
-    </div> --}}
 @endsection

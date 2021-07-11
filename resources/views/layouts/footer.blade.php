7849fb0244134e8d10433fdcc58f2ac11bbe13e6
Allen <aq98723il6732@gmail.com>
2021-07-12 00:38:37 +0800

    change footer information block width

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer id="footer" class="bg-gray-800 pt-4 mt-6">
     <div class="max-w-6xl m-auto text-gray-800 flex flex-wrap justify-left">
         {{-- Col-1 --}}
-        <div class="p-5 w-1/2 sm:w-1/3 md:w-1/4">
+        <div class="p-5 w-1/2 sm:w-1/3">
             {{-- Title --}}
             <div class="text-lg uppercase text-gray-500 font-medium mb-6">
                 About
@@ -18,7 +18,7 @@
         </div>
 
         {{-- Col-2 --}}
-        <div class="p-5 w-1/2 sm:w-1/3 md:w-1/4">
+        <div class="p-5 w-1/2 sm:w-1/3">
             {{-- Title --}}
             <div class="text-lg uppercase text-gray-500 font-medium mb-6">
                 Learing
@@ -32,7 +32,7 @@
         </div>
 
         {{-- Col-3 --}}
-        <div class="p-5 w-1/2 sm:w-1/3 md:w-1/4">
+        <div class="p-5 w-1/2 sm:w-1/3">
             {{-- Title --}}
             <div class="text-lg uppercase text-gray-500 font-medium mb-6">
                 Special Thanks

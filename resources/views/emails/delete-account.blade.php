cdc9702aa305b54080a4b7dac5e2750b2e64624c
Allen <aq98723il6732@gmail.com>
2021-07-18 22:45:02 +0800

    change the delete account email title

--- resources/views/emails/delete-account.blade.php
+++ resources/views/emails/delete-account.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>刪除用戶</title>
+    <title>刪除帳號</title>
 
     {{-- Styles --}}
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">

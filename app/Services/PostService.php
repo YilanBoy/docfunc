51d139fb3537874e73ffaf819fcc18bc4265de72
Allen <aq98723il6732@gmail.com>
2021-08-17 23:39:49 +0800

    change the rule of making slug

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -14,8 +14,11 @@ class PostService
         // 去掉特殊字元，只留中文與英文
         $title = preg_replace('/[^A-Za-z0-9 \p{Han}]+/u', '', $title);
 
-        // 去掉空白
+        // 將空白替換成 '-'
         $title = str_replace(' ', '-', $title);
+
+        // 英文全部改為小寫
+        $title = strtolower($title);
 
         // 後面加個 '-post' 是為了避免 slug 只有 'edit' 時，會與編輯頁面的路由發生衝突
         return $title . '-post';

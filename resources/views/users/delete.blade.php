3e06cf6f8b5aa7172d2bb96b5f5e55db9c19e10a
Allen <aq98723il6732@gmail.com>
2021-08-21 11:39:00 +0800

    change button sytle

--- resources/views/users/delete.blade.php
+++ resources/views/users/delete.blade.php
@@ -21,7 +21,7 @@
             <button
                 type="submit"
                 class="inline-flex justify-center items-center px-4 py-2 bg-red-600 border border-transparent rounded-md font-semibold text-white
-                uppercase tracking-widest hover:bg-red-500 active:bg-red-900 focus:outline-none focus:border-red-900
+                uppercase tracking-widest hover:bg-red-700 active:bg-red-900 focus:outline-none focus:border-red-900
                 focus:ring ring-red-300 disabled:opacity-25 transition ease-in-out duration-150"
             >
                 <i class="bi bi-exclamation-triangle-fill"></i><span class="ml-2">寄出刪除帳號信件</span>

db86b4fe319b1d8cd5bbcdcdf829bb8b6111a7c0
Allen <aq98723il6732@gmail.com>
2021-10-04 00:36:42 +0800

    add rounded style to code block

--- resources/js/app.js
+++ resources/js/app.js
@@ -4,4 +4,16 @@ import Alpine from 'alpinejs'
 
 window.Alpine = Alpine
 
+Alpine.data('cardLink', () => ({
+    // Card 連結
+    directToCardLink(event, refs) {
+        let ignores = ['a'];
+        let targetTagName = event.target.tagName.toLowerCase();
+
+        if (!ignores.includes(targetTagName)) {
+            refs.cardLinkUrl.click();
+        }
+    }
+}));
+
 Alpine.start()

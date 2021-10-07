9efc033afa48783423b51789c63e20b553057acc
Allen <aq98723il6732@gmail.com>
2021-10-07 14:05:29 +0800

    add growing bar animation

--- resources/views/users/information.blade.php
+++ resources/views/users/information.blade.php
@@ -1,20 +1,18 @@
 <x-card class="w-full mt-6 dark:text-gray-50">
 
-    <h3 class="w-full font-semibold text-lg border-black border-b-2 pb-3 mb-3
-    dark:border-white">
+    <h3 class="w-full pb-3 mb-3 text-lg font-semibold border-b-2 border-black dark:border-white">
         <span class="ml-2">個人簡介</span>
     </h3>
 
     @if ($user->introduction)
-        <span class="w-full flex justify-start items-center">{!! nl2br(e($user->introduction)) !!}</span>
+        <span class="flex items-center justify-start w-full">{!! nl2br(e($user->introduction)) !!}</span>
     @else
-        <span class="w-full flex justify-center items-center">目前尚無個人簡介～</span>
+        <span class="flex items-center justify-center w-full">目前尚無個人簡介～</span>
     @endif
 </x-card>
 
 <x-card class="w-full mt-6 dark:text-gray-50">
-    <h3 class="w-full font-semibold text-lg border-black border-b-2 pb-3 mb-3
-    dark:border-white">
+    <h3 class="w-full pb-3 mb-3 text-lg font-semibold border-b-2 border-black dark:border-white">
         <span class="ml-2">文章統計</span>
     </h3>
 
@@ -23,12 +21,12 @@
             <div class="col-span-12">
                 {{ $category->name }}
             </div>
-            <div class="col-span-11 flex items-center">
-                <div class="h-4 bg-gradient-to-r from-green-400 to-blue-400"
+            <div class="flex items-center col-span-11">
+                <div class="h-4 transition-all duration-300 rounded-sm growing-bar bg-gradient-to-r from-green-400 to-blue-400"
                 style="width: {{ $category->posts->count() ? (int)($category->posts->count() / $user->posts->count() * 100) : 0.2 }}rem;">
                 </div>
             </div>
-            <div class="col-span-1 flex justify-end items-center text-lg">
+            <div class="flex items-center justify-end col-span-1 text-lg">
                 {{ $category->posts->count() }} 篇
             </div>
         @endforeach

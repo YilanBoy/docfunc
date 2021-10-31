a9c3c08e42169ba0025c2b248116ea262e8bf59a
Allen <aq98723il6732@gmail.com>
2021-10-31 12:39:17 +0800

    add image list method and unusedImages method

--- app/Services/PostService.php
+++ app/Services/PostService.php
@@ -2,6 +2,8 @@
 
 namespace App\Services;
 
+use App\Models\Post;
+use Illuminate\Support\Facades\Storage;
 use Illuminate\Support\Str;
 use HTMLPurifier;
 use HTMLPurifier_Config;
@@ -15,7 +17,7 @@ class PostService
      * @param string $title 標題
      * @return string
      */
-    public function makeSlug(string $title): string
+    public static function makeSlug(string $title): string
     {
         // 去掉特殊字元，只留中文與英文
         $title = preg_replace('/[^A-Za-z0-9 \p{Han}]+/u', '', $title);
@@ -36,7 +38,7 @@ class PostService
      * @param string $html
      * @return string
      */
-    public function htmlPurifier(string $html): string
+    public static function htmlPurifier(string $html): string
     {
         // 設定 XSS 過濾規則
         $config = HTMLPurifier_Config::createDefault();
@@ -88,8 +90,59 @@ class PostService
      * @return string
      */
     #[Pure]
-    public function makeExcerpt(string $body, int $length = 200): string
+    public static function makeExcerpt(string $body, int $length = 200): string
     {
         return Str::limit(strip_tags($body), $length);
+    }
+
+    /**
+     * 取得所有文章中的圖片連結
+     *
+     * @return array
+     */
+    public static function imageList(): array
+    {
+        $allPostsBody = Post::pluck('body')->toArray();
+
+        $imageList = [];
+
+        foreach ($allPostsBody as $body) {
+            // 圖片名稱範例格式 images/2021_10_30_22_37_44_617d589b50eb1.jpg
+            preg_match_all(
+                '/images\/\d{4}_\d{2}_\d{2}_\d{2}_\d{2}_\d{2}_[a-zA-Z0-9]+\.(jpeg|png|jpg|gif|svg)/U',
+                $body,
+                $match
+            );
+
+            if (!empty($match[0])) {
+                foreach ($match[0] as $image) {
+                    $imageList[] = $image;
+                }
+            }
+        }
+
+        return $imageList;
+    }
+
+    /**
+     * 取得所有已未在文章中使用，但仍儲存於雲端中的圖片名稱
+     *
+     * @return array
+     */
+    public static function unusedImages(): array
+    {
+        $imageList = self::imageList();
+
+        $imagesOnCloud = Storage::disk('s3')->allFiles('images');
+
+        $unusedImages = [];
+
+        foreach ($imagesOnCloud as $image) {
+            if (!in_array($image, $imageList)) {
+                $unusedImages[] = $image;
+            }
+        }
+
+        return $unusedImages;
     }
 }

d4841ef6240415df0d1e0be1a614d374fc139f11
Allen <sharkheadgo@gmail.com>
2021-12-21 17:53:15 +0800

    change the user information page layout

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -17,45 +17,12 @@
 {{-- 個人頁面 --}}
 <x-app-layout>
     <div class="container mx-auto max-w-7xl">
-        <div class="flex flex-col items-center justify-start min-h-screen
-        px-4 mt-6 md:flex-row md:justify-center md:items-start xl:px-0">
-
-            {{-- 會員基本資訊 --}}
-            <x-card class="flex flex-col items-center justify-center w-full mr-0 md:w-80 md:mr-6 dark:text-gray-50">
-                {{-- 大頭貼 --}}
-                <div>
-                    <img
-                        class="rounded-full h-36 w-36"
-                        src="{{ $user->gravatar }}"
-                        alt="{{ $user->name }}"
-                        width="200"
-                    >
-                </div>
-
-                {{-- 會員名稱 --}}
-                <span class="flex items-center justify-center w-full pb-5 my-5 text-3xl font-semibold
-                border-b-2 border-black dark:text-gray-50 dark:border-white">
-                    {{ $user->name }}
-                </span>
-
-                {{-- 資訊 --}}
-                <div class="flex flex-col items-start justify-start w-full dark:text-gray-50">
-                    <span class="text-lg">撰寫文章</span>
-                    <span class="mt-2 text-xl font-semibold">{{ $user->posts->count() }} 篇</span>
-
-                    <span class="mt-4 text-lg">文章留言</span>
-                    <span class="mt-2 text-xl font-semibold">{{ $user->comments->count() }} 次</span>
-
-                    <span class="mt-4 text-xs">
-                        註冊於 {{ $user->created_at->format('Y / m / d') . '（' . $user->created_at->diffForHumans() . '）' }}
-                    </span>
-                </div>
-            </x-card>
+        <div class="flex flex-col items-center justify-start min-h-screen mt-6 px-4">
 
             {{-- 會員資訊、文章與留言 --}}
             <div
                 x-data="{ tab: 'information' }"
-                class="w-full mt-4 space-y-6 xl:w-2/3 md:mt-0"
+                class="w-full lg:w-2/3 space-y-6"
             >
                 {{-- 切換顯示選單 --}}
                 <nav class="flex w-full p-1 space-x-1 md:w-4/5 lg:w-1/2

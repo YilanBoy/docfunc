730804ed9362b0d4d1ab97b2e991df7ab2e07a7c
Allen <sharkheadgo@gmail.com>
2021-12-26 21:50:55 +0800

    add icon to learning resource list

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -70,7 +70,7 @@
                   rel="nofollow noopener noreferrer"
                   class="block p-2 rounded-md hover:bg-gray-200 dark:text-gray-50 dark:hover:bg-gray-600"
                 >
-                  {{ $link->title }}
+                  <i class="bi bi-link-45deg"></i><span class="ml-2">{{ $link->title }}</span>
                 </a>
               @endforeach
             </div>

31f8c3c739c7265bdf423708d0c4180297a80d72
Allen <sharkheadgo@gmail.com>
2022-01-09 20:59:12 +0800

    change button style

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -57,7 +57,7 @@
                 {{-- 編輯文章 --}}
                 <a
                   href="{{ route('posts.edit', ['post' => $post->id]) }}"
-                  class="inline-flex items-center justify-center w-16 h-16 transition duration-150 ease-in-out bg-green-500 border border-transparent group rounded-xl text-gray-50 hover:bg-green-600 active:bg-green-700 focus:outline-none focus:border-green-700 focus:ring ring-green-300"
+                  class="inline-flex items-center justify-center w-16 h-16 transition duration-150 ease-in-out border border-transparent bg-emerald-500 group rounded-xl text-gray-50 hover:bg-emerald-600 active:bg-emerald-700 focus:outline-none focus:border-emerald-700 focus:ring ring-emerald-300"
                 >
                   <span class="text-2xl transition duration-150 ease-in group-hover:scale-125 group-hover:-rotate-12">
                     <i class="bi bi-pencil-fill"></i>

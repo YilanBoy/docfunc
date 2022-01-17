cfed715f1b802c9dcdb158b24dd8cdfddec98363
Allen <sharkheadgo@gmail.com>
2022-01-18 00:49:29 +0800

    change floating labels style

--- resources/views/components/floating-label-input.blade.php
+++ resources/views/components/floating-label-input.blade.php
@@ -1,16 +1,16 @@
-<div class="relative px-4 pt-6 pb-4 rounded-lg bg-slate-100 dark:bg-slate-600">
+<div class="relative px-2 pt-5 pb-2 rounded-lg bg-slate-100 dark:bg-slate-600">
   <input
     type="{{ $type }}"
     name="{{ $name }}"
     value="{{ $value ?? '' }}"
     placeholder="{{ $placeholder }}"
     {{ $attributes }}
-    class="w-full h-10 text-gray-900 placeholder-transparent transition duration-150 ease-in bg-transparent border-b-2 border-gray-300 peer focus:outline-none focus:border-blue-500 dark:text-gray-50"
+    class="w-full h-10 text-gray-900 placeholder-transparent transition duration-150 ease-in bg-transparent border-b-2 border-gray-300 peer focus:outline-none focus:border-blue-500 dark:text-gray-50 dark:border-gray-400 dark:focus:border-red-500"
   >
 
   <label
     for="{{ $name }}"
-    class="absolute text-sm text-gray-600 transition-all pointer-events-none left-4 top-2 dark:text-gray-50 peer-placeholder-shown:text-lg peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-6 peer-focus:top-2 peer-focus:text-gray-600 peer-focus:text-sm selection:dark:text-gray-50 dark:peer-placeholder-shown:text-gray-50 dark:peer-focus:text-gray-50"
+    class="absolute text-sm text-gray-600 transition-all pointer-events-none top-1 left-2 dark:text-gray-50 peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-400 peer-placeholder-shown:top-5 peer-focus:top-1 peer-focus:text-gray-600 peer-focus:text-sm selection:dark:text-gray-50 dark:peer-placeholder-shown:text-gray-400 dark:peer-focus:text-gray-50"
   >
     {{ $placeholder }}
   </label>

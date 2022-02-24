7b9fba5b103e136207464e3565d603f14e60a875
Allen <sharkheadgo@gmail.com>
2022-02-25 00:03:28 +0800

    change tag style

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,6 +1,9 @@
-<a
-  href="{{ $href }}"
-  class="inline-flex items-center px-3 py-1 m-1 text-xs font-bold text-gray-700 uppercase rounded-full bg-zinc-200 hover:bg-zinc-300 ring-1 ring-zinc-700 dark:text-gray-50 dark:bg-zinc-600 dark:hover:bg-zinc-500 dark:ring-zinc-50"
->
-  {{ $slot }}
-</a>
+<div
+  class="p-0.5 bg-gradient-to-br from-cyan-500 to-blue-500 rounded-full flex m-1 items-center justify-center">
+  <a
+    href="{{ $href }}"
+    class="inline-flex items-center px-3 py-1 text-xs font-bold text-gray-700 uppercase rounded-full bg-zinc-200 hover:bg-zinc-300 dark:text-gray-50 dark:bg-zinc-600 dark:hover:bg-zinc-500"
+  >
+    {{ $slot }}
+  </a>
+</div>

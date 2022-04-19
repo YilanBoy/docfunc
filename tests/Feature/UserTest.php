7e75548019f876b9840cd01e50c558301fc675be
Allen <aq98723il6732@gmail.com>
2022-04-20 00:33:01 +0800

    add change password test and send delete account test

--- tests/Feature/UserTest.php
+++ tests/Feature/UserTest.php
@@ -2,9 +2,12 @@
 
 namespace Tests\Feature;
 
+use App\Mail\DestroyUser;
 use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
 use Tests\TestCase;
 
 class UserTest extends TestCase
@@ -60,5 +63,95 @@ class UserTest extends TestCase
             'introduction' => $this->faker->realText(119),
         ])
             ->assertSessionHasErrors('name');
+    }
+
+    public function test_user_can_visit_change_password_page()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $this->get(route('users.changePassword', $user->id))
+            ->assertSuccessful();
+    }
+
+    public function test_user_can_change_password()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $this->put(route('users.changePassword', $user->id), [
+            'current_password' => 'Password101',
+            'new_password' => 'NewPassword101',
+            'new_password_confirmation' => 'NewPassword101',
+        ])
+            ->assertStatus(302)
+            ->assertSessionHas('status', '密碼修改成功！');
+    }
+
+    public function test_user_can_visit_delete_account_page()
+    {
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $this->get(route('users.delete', $user->id))
+            ->assertSuccessful();
+    }
+
+    public function test_send_destroy_user_email_queue()
+    {
+        Mail::fake();
+
+        $user = User::factory()->create();
+
+        $this->actingAs($user);
+
+        $this->post(route('users.sendDestroyEmail', $user->id))
+            ->assertStatus(302);
+
+        Mail::assertQueued(DestroyUser::class);
+    }
+
+    public function test_user_can_delete_own_account()
+    {
+        $user = User::factory()->create();
+
+        $this->assertDatabaseHas('users', ['id' => $user->id]);
+
+        $this->actingAs($user);
+
+        $destroyUserLink = URL::temporarySignedRoute(
+            'users.destroy',
+            now()->addMinutes(5),
+            ['user' => $user->id]
+        );
+
+        $this->get($destroyUserLink)->assertStatus(302);
+
+        $this->assertDatabaseMissing('users', ['id' => $user->id]);
+    }
+
+    public function test_user_can_not_delete_own_account_if_url_is_invalid()
+    {
+        $user = User::factory()->create();
+
+        $this->assertDatabaseHas('users', ['id' => $user->id]);
+
+        $this->actingAs($user);
+
+        $destroyUserLink = URL::temporarySignedRoute(
+            'users.destroy',
+            now()->addMinutes(5),
+            ['user' => $user->id]
+        );
+
+        // 讓時間經過 6 分鐘，使連結失效
+        $this->travel(6)->minutes();
+
+        $this->get($destroyUserLink)->assertStatus(401);
+
+        $this->assertDatabaseHas('users', ['id' => $user->id]);
     }
 }

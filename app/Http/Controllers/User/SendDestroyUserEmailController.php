c9f9e616e2cb638093bff3ed0bbc67fef30da50b
Allen <aq98723il6732@gmail.com>
2022-04-20 00:31:29 +0800

    use queue to send email

--- app/Http/Controllers/User/SendDestroyUserEmailController.php
+++ app/Http/Controllers/User/SendDestroyUserEmailController.php
@@ -3,10 +3,12 @@
 namespace App\Http\Controllers\User;
 
 use App\Http\Controllers\Controller;
-use App\Jobs\SendDestroyUserEmail;
+use App\Mail\DestroyUser;
 use App\Models\User;
 use Illuminate\Auth\Access\AuthorizationException;
 use Illuminate\Http\RedirectResponse;
+use Illuminate\Support\Facades\Mail;
+use Illuminate\Support\Facades\URL;
 
 class SendDestroyUserEmailController extends Controller
 {
@@ -21,7 +23,14 @@ class SendDestroyUserEmailController extends Controller
     {
         $this->authorize('update', $user);
 
-        SendDestroyUserEmail::dispatch($user);
+        // 生成一次性連結，並設定 5 分鐘後失效
+        $destroyUserLink = URL::temporarySignedRoute(
+            'users.destroy',
+            now()->addMinutes(5),
+            ['user' => $user->id]
+        );
+
+        Mail::to($user)->queue(new DestroyUser($destroyUserLink));
 
         return back()->with('status', '已寄出信件！');
     }

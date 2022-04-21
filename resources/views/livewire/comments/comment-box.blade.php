aaa8908735b67c753577e778c9eb4d98804a2343
Allen <aq98723il6732@gmail.com>
2022-04-22 00:42:47 +0800

    change button style

--- resources/views/livewire/comments/comment-box.blade.php
+++ resources/views/livewire/comments/comment-box.blade.php
@@ -22,9 +22,9 @@
         $nextTick(() => { $refs.commentBox.focus() })
       "
       type="button"
-      class="group [transform:translateZ(0)] px-6 py-3 rounded-xl bg-emerald-500 overflow-hidden relative before:absolute before:bg-blue-600 before:bottom-0 before:left-0 before:h-full before:w-full before:origin-[100%_100%] before:scale-x-0 hover:before:origin-[0_0] hover:before:scale-x-100 before:transition before:ease-in-out before:duration-500"
+      class="group [transform:translateZ(0)] px-4 py-2 rounded-lg bg-emerald-500 overflow-hidden relative before:absolute before:bg-blue-600 before:bottom-0 before:left-0 before:h-full before:w-full before:origin-[100%_100%] before:scale-x-0 hover:before:origin-[0_0] hover:before:scale-x-100 before:transition before:ease-in-out before:duration-500"
     >
-      <span class="relative z-0 text-xl font-semibold text-gray-200 transition duration-500 ease-in-out">
+      <span class="relative z-0 text-lg font-semibold text-gray-200 transition duration-500 ease-in-out">
         <i class="bi bi-pencil-fill"></i><span class="ml-2">新增留言</span>
       </span>
     </button>

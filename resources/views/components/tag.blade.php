cd3ebb179c7a52434ace9ea83522b7e1b6709bff
Allen <aq98723il6732@gmail.com>
2022-05-13 16:45:27 +0800

    change tag bg color

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -2,7 +2,7 @@
   class="p-0.5 bg-gradient-to-br from-cyan-500 to-blue-500 rounded-full flex m-1 items-center justify-center">
   <a
     href="{{ $href }}"
-    class="inline-flex items-center px-3 py-1 text-xs font-bold text-gray-700 uppercase rounded-full bg-slate-200 hover:bg-slate-300 dark:text-gray-50 dark:bg-slate-600 dark:hover:bg-slate-500"
+    class="inline-flex items-center px-3 py-1 text-xs font-bold text-gray-700 uppercase transition-colors duration-150 ease-in rounded-full bg-slate-200 hover:bg-slate-300 dark:text-gray-50 dark:bg-slate-700 dark:hover:bg-slate-500"
   >
     {{ $slot }}
   </a>

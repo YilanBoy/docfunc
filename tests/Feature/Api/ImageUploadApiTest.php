f7a9b7fab65337bfb27f095b29d540b4a5140e5f
Allen <aq98723il6732@gmail.com>
2022-09-13 21:43:31 +0800

    add image upload api test

---
+++ tests/Feature/Api/ImageUploadApiTest.php
@@ -0,0 +1,37 @@
+<?php
+
+use App\Models\User;
+use Illuminate\Http\UploadedFile;
+
+test('guest can\'t upload image', function () {
+    Storage::fake('s3');
+
+    $this->post(route('images.store'), [
+        'upload' => UploadedFile::fake()->image('photo.jpg')->size(100),
+    ])
+        ->assertStatus(302)
+        ->assertRedirect(route('login'));
+
+    Storage::disk('s3')->assertDirectoryEmpty('image');
+});
+
+test('login user can upload image', function () {
+    $this->actingAs(User::factory()->create());
+
+    Storage::fake('s3');
+
+    $this->post(route('images.store'), [
+        'upload' => UploadedFile::fake()->image('photo.jpg')->size(100),
+    ])
+        ->assertStatus(200);
+});
+
+test('image must smaller than 512 kb', function () {
+    $this->actingAs(User::factory()->create());
+
+    Storage::fake('s3');
+
+    $this->post(route('images.store'), [
+        'upload' => UploadedFile::fake()->image('photo.jpg')->size(513),
+    ])->assertStatus(302);
+});

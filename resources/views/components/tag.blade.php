0a1783c8ac60c1e79e8d8d2f2ff8f54cf865c00f
Allen Chiang <aq98723il6732@gmail.com>
2022-12-22 00:05:07 +0800

    refactor: uniform color style
    change tag, heading and link color

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,7 +1,7 @@
 <a
   href="{{ $href }}"
-  class="inline-flex items-center px-3 py-1 text-xs font-bold text-green-700 dark:text-indigo-700 text-b transition-colors duration-150 ease-in
-  rounded-full bg-green-200 hover:bg-green-300 dark:bg-indigo-200 dark:hover:bg-indigo-300 m-1"
+  class="inline-flex items-center px-3 py-1 text-xs font-bold text-green-700 dark:text-indigo-800 transition-colors duration-150 ease-in
+  rounded-full bg-green-200 hover:bg-green-300 dark:bg-indigo-300 dark:hover:bg-indigo-400 m-1"
 >
   {{ $slot }}
 </a>

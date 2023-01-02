d9a6d63c1dd1e724ede296adbfcf470bcec95d06
Allen Chiang <aq98723il6732@gmail.com>
2023-01-02 15:12:01 +0800

    refactor: adjust test file
    - fix typo in image upload api test
    - move user console test file into console directory

--- tests/Feature/Api/ImageUploadApiTest.php
+++ tests/Feature/Api/ImageUploadApiTest.php
@@ -6,22 +6,20 @@ use Illuminate\Http\UploadedFile;
 
 use function Pest\Laravel\post;
 
-test('guest can\'t upload image', function () {
-    Storage::fake('s3');
+beforeEach(fn () => Storage::fake('s3'));
 
+test('guest can\'t upload image', function () {
     post(route('images.store'), [
         'upload' => UploadedFile::fake()->image('photo.jpg')->size(100),
     ])
         ->assertStatus(302)
         ->assertRedirect(route('login'));
 
-    Storage::disk('s3')->assertDirectoryEmpty('image');
+    Storage::disk('s3')->assertDirectoryEmpty('images');
 });
 
 test('login user can upload image', function () {
     $this->actingAs(User::factory()->create());
-
-    Storage::fake('s3');
 
     $file = UploadedFile::fake()->image('photo.jpg')->size(100);
     $filename = 'fake_converted_image.jpg';
@@ -34,14 +32,16 @@ test('login user can upload image', function () {
     post(route('images.store'), [
         'upload' => $file,
     ])->assertStatus(200);
+
+    Storage::disk('s3')->assertExists('images/'.$filename);
 });
 
 test('image must smaller than 512 kb', function () {
     $this->actingAs(User::factory()->create());
 
-    Storage::fake('s3');
-
     post(route('images.store'), [
         'upload' => UploadedFile::fake()->image('photo.jpg')->size(513),
     ])->assertStatus(302);
+
+    Storage::disk('s3')->assertDirectoryEmpty('images');
 });

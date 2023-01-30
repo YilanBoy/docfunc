5ed641f367aed032bd49c45ece6e604799f6ff84
Allen Chiang <aq98723il6732@gmail.com>
2023-01-30 21:08:17 +0800

    refactor: adjust some rule
    - email mask rule
    - recaptcha pass score

--- config/services.php
+++ config/services.php
@@ -33,6 +33,6 @@ return [
     'recaptcha' => [
         'site_key' => env('RECAPTCHA_SITE_KEY'),
         'secret_key' => env('RECAPTCHA_SECRET_KEY'),
-        'min_score' => env('RECAPTCHA_MIN_SCORE', .5),
+        'min_score' => env('RECAPTCHA_MIN_SCORE', .7),
     ],
 ];
--- resources/views/livewire/users/edit/edit-information.blade.php
+++ resources/views/livewire/users/edit/edit-information.blade.php
@@ -34,11 +34,17 @@
         class="text-gray-600 dark:text-gray-50"
       >信箱</label>
 
+      @php
+        $emailLength = strlen($user->email);
+        $startToMask = round($emailLength / 4);
+        $maskLength = ceil($emailLength / 2)
+      @endphp
+
       <input
         id="email"
         type="text"
         name="email"
-        value="{{ str()->mask($user->email, '*', 1, 5) }}"
+        value="{{ str()->mask($user->email, '*', $startToMask, $maskLength) }}"
         placeholder="信箱"
         disabled
         class="mt-2 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 disabled:bg-slate-50 disabled:text-slate-500 disabled:border-slate-200 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md focus:ring-1 invalid:border-pink-500 invalid:text-pink-600 focus:invalid:border-pink-500 focus:invalid:ring-pink-500 disabled:shadow-none dark:disabled:bg-slate-700 dark:disabled:text-slate-400 dark:disabled:border-slate-500"

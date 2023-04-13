85572bf28b9cf2389737d54f9edeec4b6ea3b118
Allen Chiang <aq98723il6732@gmail.com>
2023-04-13 16:15:22 +0800

    feature: add dockerfile syntax highlight and update horizon assets

--- resources/ts/highlight.ts
+++ resources/ts/highlight.ts
@@ -2,12 +2,14 @@
 import hljs from "highlight.js/lib/common";
 // 加入 Dart
 import dart from "highlight.js/lib/languages/dart";
+import dockerfile from "highlight.js/lib/languages/dockerfile";
 
 interface Window {
     hljs: any;
 }
 
 hljs.registerLanguage("dart", dart);
+hljs.registerLanguage("dockerfile", dockerfile);
 
 hljs.highlightAll();
 

f3456b67b7c47de300f0b132a9a5e6036c359d28
Allen Chiang <aq98723il6732@gmail.com>
2023-05-03 17:24:42 +0800

    refactor: refactor h2 style

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,61 +1,61 @@
-const defaultTheme = require("tailwindcss/defaultTheme");
+const defaultTheme = require('tailwindcss/defaultTheme');
 
 module.exports = {
-    darkMode: "class",
+    darkMode: 'class',
     content: [
-        "./vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php",
-        "./storage/framework/views/*.php",
-        "./resources/views/**/*.blade.php",
-        "./resources/js/*.js",
-        "./resources/ts/*.ts",
+        './vendor/laravel/framework/src/Illuminate/Pagination/resources/views/*.blade.php',
+        './storage/framework/views/*.php',
+        './resources/views/**/*.blade.php',
+        './resources/js/*.js',
+        './resources/ts/*.ts',
     ],
     theme: {
         extend: {
             fontFamily: {
-                sans: ["Nunito", ...defaultTheme.fontFamily.sans],
+                sans: ['Nunito', ...defaultTheme.fontFamily.sans],
                 noto: "'Noto Sans TC', sans-serif",
             },
             keyframes: {
-                "fade-in": {
+                'fade-in': {
                     from: {
                         opacity: 0,
-                        transform: "translateY(20px)",
+                        transform: 'translateY(20px)',
                     },
                     to: {
                         opacity: 1,
-                        transform: "translateY(0)",
+                        transform: 'translateY(0)',
                     },
                 },
-                "grow-width": {
+                'grow-width': {
                     from: {
                         width: 0,
                     },
                     to: {
-                        width: "100%",
+                        width: '100%',
                     },
                 },
             },
             animation: {
-                "fade-in": "fade-in 0.5s ease-in-out",
-                "grow-width": "grow-width 1s forwards",
+                'fade-in': 'fade-in 0.5s ease-in-out',
+                'grow-width': 'grow-width 1s forwards',
             },
             typography: (theme) => ({
                 DEFAULT: {
                     css: {
                         // blockquote
-                        "blockquote p:first-of-type::before": null,
-                        "blockquote p:last-of-type::after": null,
+                        'blockquote p:first-of-type::before': null,
+                        'blockquote p:last-of-type::after': null,
                         // inline code
-                        ":not(pre) > code": {
-                            backgroundColor: theme("colors.green.100"),
-                            color: theme("colors.green.600"),
-                            padding: "0.25rem",
-                            fontWeight: "600",
-                            borderRadius: "0.25rem",
+                        ':not(pre) > code': {
+                            backgroundColor: theme('colors.green.100'),
+                            color: theme('colors.green.600'),
+                            padding: '0.25rem',
+                            fontWeight: '600',
+                            borderRadius: '0.25rem',
                         },
-                        ".dark :not(pre) > code": {
-                            backgroundColor: theme("colors.gray.500"),
-                            color: theme("colors.gray.100"),
+                        '.dark :not(pre) > code': {
+                            backgroundColor: theme('colors.gray.500'),
+                            color: theme('colors.gray.100'),
                         },
                     },
                 },
@@ -63,9 +63,9 @@ module.exports = {
         },
     },
     plugins: [
-        require("@tailwindcss/forms")({
-            strategy: "class",
+        require('@tailwindcss/forms')({
+            strategy: 'class',
         }),
-        require("@tailwindcss/typography"),
+        require('@tailwindcss/typography'),
     ],
 };

845ac46576bf78ceee030e16448c6f44796fc208
allenjiang <allenjianggo@gmail.com>
2023-08-12 18:50:03 +0800

    refactor: update button style

--- resources/views/components/button.blade.php
+++ resources/views/components/button.blade.php
@@ -2,7 +2,7 @@
   {{ $attributes->merge([
       'type' => 'submit',
       'class' =>
-          'inline-flex items-center justify-center rounded-xl border border-transparent bg-green-600 px-4 py-2 font-semibold uppercase tracking-widest text-gray-50 ring-green-300 transition duration-150 ease-in-out hover:bg-green-700 focus:border-green-700 focus:outline-none focus:ring active:bg-green-600 disabled:opacity-25 dark:bg-[#1b87f5] dark:hover:bg-[#44adff] dark:ring-[#8edaff] dark:focus:border-[#146fe1] dark:active:bg-[#1b87f5]',
+          'inline-flex items-center justify-center rounded-xl border border-transparent bg-green-600 px-4 py-2 font-semibold uppercase tracking-widest text-gray-50 ring-green-300 transition duration-150 ease-in-out hover:bg-green-700 focus:border-green-700 focus:outline-none focus:ring active:bg-green-600 disabled:opacity-25 dark:bg-lividus-600 dark:ring-lividus-400 dark:hover:bg-lividus-500 dark:focus:border-lividus-700 dark:active:bg-lividus-600',
   ]) }}
 >
   {{ $slot }}

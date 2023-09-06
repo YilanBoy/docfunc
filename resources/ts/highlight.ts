bba9698fb8ad03b9f1f5e0ee41ad760f75e7bf8f
allenjiang <allenjianggo@gmail.com>
2023-09-07 03:14:28 +0800

    feature: add terraform syntax highlight

--- resources/ts/highlight.ts
+++ resources/ts/highlight.ts
@@ -5,6 +5,83 @@ import dart from 'highlight.js/lib/languages/dart';
 import dockerfile from 'highlight.js/lib/languages/dockerfile';
 import nginx from 'highlight.js/lib/languages/nginx';
 
+// https://github.com/highlightjs/highlightjs-terraform/blob/master/terraform.js
+function terraform(hljs: any) {
+    const NUMBERS = {
+        className: 'number',
+        begin: '\\b\\d+(\\.\\d+)?',
+        relevance: 0,
+    };
+    const STRINGS = {
+        className: 'string',
+        begin: '"',
+        end: '"',
+        contains: [
+            {
+                className: 'variable',
+                begin: '\\${',
+                end: '\\}',
+                relevance: 9,
+                contains: [
+                    {
+                        className: 'string',
+                        begin: '"',
+                        end: '"',
+                    },
+                    {
+                        className: 'meta',
+                        begin: '[A-Za-z_0-9]*' + '\\(',
+                        end: '\\)',
+                        contains: [
+                            NUMBERS,
+                            {
+                                className: 'string',
+                                begin: '"',
+                                end: '"',
+                                contains: [
+                                    {
+                                        className: 'variable',
+                                        begin: '\\${',
+                                        end: '\\}',
+                                        contains: [
+                                            {
+                                                className: 'string',
+                                                begin: '"',
+                                                end: '"',
+                                                contains: [
+                                                    {
+                                                        className: 'variable',
+                                                        begin: '\\${',
+                                                        end: '\\}',
+                                                    },
+                                                ],
+                                            },
+                                            {
+                                                className: 'meta',
+                                                begin: '[A-Za-z_0-9]*' + '\\(',
+                                                end: '\\)',
+                                            },
+                                        ],
+                                    },
+                                ],
+                            },
+                            'self',
+                        ],
+                    },
+                ],
+            },
+        ],
+    };
+
+    return {
+        aliases: ['tf', 'hcl'],
+        keywords:
+            'resource variable provider output locals module data terraform|10',
+        literal: 'false true null',
+        contains: [hljs.COMMENT('\\#', '$'), NUMBERS, STRINGS],
+    };
+}
+
 declare global {
     interface Window {
         hljs: any;
@@ -14,6 +91,7 @@ declare global {
 hljs.registerLanguage('dart', dart);
 hljs.registerLanguage('dockerfile', dockerfile);
 hljs.registerLanguage('nginx', nginx);
+hljs.registerLanguage('terraform', terraform);
 
 // hljs.highlightAll();
 

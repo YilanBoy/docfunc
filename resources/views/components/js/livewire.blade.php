6bde404c95091474cd4dce7d3b00aef1ed6305d2
Allen Chiang <allenjianggo@gmail.com>
2023-10-17 12:55:55 +0800

    refactor: remove unnecessary code

--- resources/views/components/js/livewire.blade.php
+++ resources/views/components/js/livewire.blade.php
@@ -3,41 +3,6 @@
     // Runs after Livewire is loaded but before it's initialized
     // on the page...
 
-    Livewire.directive("confirm", ({
-      el,
-      directive,
-      component,
-      cleanup
-    }) => {
-      let content = directive.expression;
-
-      // The "directive" object gives you access to the parsed directive.
-      // For example, here are its values for: wire:click.prevent="deletePost(1)"
-      //
-      // directive.raw = wire:click.prevent
-      // directive.value = "click"
-      // directive.modifiers = ['prevent']
-      // directive.expression = "deletePost(1)"
-
-      let onClick = e => {
-        if (!confirm(content)) {
-          e.preventDefault();
-          e.stopPropagation();
-        }
-      };
-
-      el.addEventListener("click", onClick, {
-        capture: true
-      });
-
-      // Register any cleanup code inside `cleanup()` in the case
-      // where a Livewire component is removed from the DOM while
-      // the page is still active.
-      cleanup(() => {
-        el.removeEventListener("click", onClick);
-      });
-    });
-
     Livewire.hook("commit", ({
       component,
       commit,

6365c91d0f7e77e1052c286a659e47bc4e2feee3
Allen Chiang <allenjianggo@gmail.com>
2023-11-09 09:48:13 +0800

    refactor: update tag style

--- resources/views/components/tag.blade.php
+++ resources/views/components/tag.blade.php
@@ -1,5 +1,5 @@
 <a
-  class="m-1 inline-flex items-center rounded-full bg-green-200 px-2.5 py-0.5 text-xs font-semibold text-green-900 transition-colors duration-150 ease-in hover:bg-green-300 dark:bg-lividus-600 dark:text-gray-50 dark:hover:bg-lividus-500"
+  class="m-1 inline-flex items-center rounded-full bg-green-200 px-2.5 py-0.5 text-xs text-green-900 transition-colors duration-150 ease-in hover:bg-green-300 dark:bg-lividus-600 dark:text-gray-50 dark:hover:bg-lividus-500"
   href="{{ $href }}"
   wire:navigate
 >

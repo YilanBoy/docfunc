335534a8d24d6a90145639b096d58118472956b0
Allen Chiang <allenjianggo@gmail.com>
2023-12-21 11:43:01 +0800

    fix: use local disk for temporary image

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,6 +2,7 @@
 
 namespace App\Providers;
 
+use Config;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\ServiceProvider;
 
@@ -21,5 +22,7 @@ class AppServiceProvider extends ServiceProvider
     public function boot(): void
     {
         Model::preventLazyLoading(! app()->isProduction());
+
+        Config::set('livewire.temporary_file_upload.disk', 'local');
     }
 }

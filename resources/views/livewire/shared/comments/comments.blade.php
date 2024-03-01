57ebc04f2f30d1fb40f3948c7b9c7b0f74b74bfa
Allen Chiang <allenjianggo@gmail.com>
2024-03-01 17:58:54 +0800

    refactor: remove unnecessary code

--- resources/views/livewire/shared/comments/comments.blade.php
+++ resources/views/livewire/shared/comments/comments.blade.php
@@ -3,9 +3,13 @@
     Alpine.data('comments', () => ({
       currentScrollY: 0,
       init() {
-        Livewire.hook('commit.prepare', () => {
-          this.currentScrollY = window.scrollY;
-        })
+        Livewire.hook('commit.prepare', ({
+          component
+        }) => {
+          if (component.name === 'shared.comments.comments') {
+            this.currentScrollY = window.scrollY;
+          }
+        });
 
         Livewire.hook('morph.updated', ({
           component
@@ -13,7 +17,7 @@
           if (component.name === 'shared.comments.comments') {
             window.scrollTo(0, this.currentScrollY);
           }
-        })
+        });
 
         let commentsObserver = new MutationObserver(() => {
           this.$refs.comments
@@ -65,40 +69,17 @@
   @endforeach
 
   @if ($showMoreButtonIsActive)
-    <div class="mt-6 flex items-center justify-center">
+    <div class="mt-6 flex w-full justify-center">
       <button
-        class="relative text-lg dark:text-gray-50"
+        class="flex items-center text-lg dark:text-gray-50"
         type="button"
-        {{-- when click the button and update the DOM, make windows.scrollY won't change --}}
         wire:click="showMore"
       >
-        <span>顯示更多</span>
-        <div
-          class="absolute -right-8 top-1.5"
+        <x-icon.animate-spin
+          class="w-5"
           wire:loading
-        >
-          <svg
-            class="h-5 w-5 animate-spin text-gray-700 dark:text-gray-50"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 24 24"
-          >
-            <circle
-              class="opacity-25"
-              cx="12"
-              cy="12"
-              r="10"
-              stroke="currentColor"
-              stroke-width="4"
-            ></circle>
-            <path
-              class="opacity-75"
-              fill="currentColor"
-              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
-            >
-            </path>
-          </svg>
-        </div>
+        />
+        <span class="ml-2">顯示更多</span>
       </button>
     </div>
   @endif

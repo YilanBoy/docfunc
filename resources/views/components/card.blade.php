122983eed5dd2d013a7b0d41e98ebaca3c26ad7d
Allen Chiang <allenjianggo@gmail.com>
2024-07-17 14:45:39 +0800

    refactor: update card style

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,3 +1,3 @@
-<div {{ $attributes->merge(['class' => 'rounded-xl bg-gray-50 p-5 dark:bg-gray-800']) }}>
+<div {{ $attributes->merge(['class' => 'rounded-xl bg-gray-50 p-4 dark:bg-gray-800']) }}>
   {{ $slot }}
 </div>

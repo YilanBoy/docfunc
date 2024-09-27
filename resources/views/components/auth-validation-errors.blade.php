e0d7b041f8c6b97b8deaef74c99c096238843119
Allen Chiang <allenjianggo@gmail.com>
2024-09-27 18:25:42 +0800

    refactor: update error badge style

--- resources/views/components/auth-validation-errors.blade.php
+++ resources/views/components/auth-validation-errors.blade.php
@@ -1,7 +1,9 @@
 @props(['errors'])
 
 @if ($errors->any())
-  <div {{ $attributes->merge(['class' => 'text-gray-700 px-4 py-2 bg-red-200 border-l-4 border-red-400']) }}>
+  <div
+    {{ $attributes->merge(['class' => 'text-red-700 dark:text-red-400 rounded-md px-4 py-2 bg-red-300/20 border-l-4 border-red-400']) }}
+  >
     <ul class="list-inside list-disc">
       @foreach ($errors->all() as $error)
         <li>{{ $error }}</li>

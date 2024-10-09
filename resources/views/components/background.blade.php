6a30588b75137b550658a58a56afaead7398c910
allenjiang <allenjianggo@gmail.com>
2024-10-09 21:23:45 +0800

    refactor: update background component

--- resources/views/components/background.blade.php
+++ resources/views/components/background.blade.php
@@ -1,80 +1,80 @@
 <div class="absolute right-1/2 top-0 -z-10 h-screen w-full max-w-[1920px] translate-x-1/2">
   {{-- icon background left side --}}
   <div class="absolute left-[2%] top-[6rem] hidden md:block">
-    <x-icon.controller class="size-64 rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.controller class="size-64 rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute left-[24%] top-[18rem] hidden lg:block">
-    <x-icon.person-walking class="size-24 -rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.person-walking class="size-24 -rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute left-[8%] top-[26rem] hidden lg:block">
-    <x-icon.rocket-takeoff class="size-24 scale-x-[-1] text-gray-300/60 dark:text-gray-800" />
+    <x-icon.rocket-takeoff class="size-24 scale-x-[-1] text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute left-[16%] top-[32rem] hidden md:block">
-    <x-icon.chat-heart class="size-64 -rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.chat-heart class="size-64 -rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute left-[2%] top-[38rem] hidden lg:block">
-    <x-icon.easel class="size-24 rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.easel class="size-24 rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute left-[10%] top-[48rem] hidden lg:block">
-    <x-icon.globe-americas class="size-24 -rotate-12 scale-x-[-1] text-gray-300/60 dark:text-gray-800" />
+    <x-icon.globe-americas class="size-24 -rotate-12 scale-x-[-1] text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   {{-- icon background center --}}
   <div class="absolute left-[24%] top-[4rem] hidden lg:block">
-    <x-icon.hand-thumbs-up class="size-24 rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.hand-thumbs-up class="size-24 rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute left-[36%] top-[10rem] hidden lg:block">
-    <x-icon.lightbulb class="size-24 -rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.lightbulb class="size-24 -rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute left-[36%] top-[26rem] hidden lg:block">
-    <x-icon.battery-charging class="size-24 -rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.battery-charging class="size-24 -rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute left-[38%] top-[40rem] hidden lg:block">
-    <x-icon.music-note-beamed class="size-24 rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.music-note-beamed class="size-24 rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute right-[36%] top-[40rem] hidden lg:block">
-    <x-icon.filetype-js class="size-24 -rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.filetype-js class="size-24 -rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute right-[36%] top-[6rem] hidden lg:block">
-    <x-icon.floppy class="size-64 rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.floppy class="size-64 rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute right-[46%] top-[32rem] hidden lg:block">
-    <x-icon.clipboard-check class="size-24 rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.clipboard-check class="size-24 rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   {{-- icon background right side --}}
   <div class="absolute right-[22%] top-[8rem] hidden lg:block">
-    <x-icon.bug class="size-24 rotate-45 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.bug class="size-24 rotate-45 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute right-[4%] top-[12rem] hidden lg:block">
-    <x-icon.folder-open class="size-24 rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.folder-open class="size-24 rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute right-[14%] top-[18rem] hidden md:block">
-    <x-icon.laptop class="size-64 -rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.laptop class="size-64 -rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute right-[4%] top-[32rem] hidden lg:block">
-    <x-icon.cpu class="size-24 -rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.cpu class="size-24 -rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute right-[20%] top-[38rem] hidden lg:block">
-    <x-icon.terminal class="size-24 rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.terminal class="size-24 rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 
   <div class="absolute right-[4%] top-[44rem] hidden md:block">
-    <x-icon.filetype-php class="size-64 rotate-12 text-gray-300/60 dark:text-gray-800" />
+    <x-icon.filetype-php class="size-64 rotate-12 text-gray-300/60 dark:text-gray-950/60" />
   </div>
 </div>
